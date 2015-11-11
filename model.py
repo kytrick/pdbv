3a8d23567c42e9357e94b4abde0cd1a0e3ffe2c4
Kayla Smith <kytrick@gmail.com>
2015-11-11 15:07:50 -0800

    added serialization

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import json
+
 
 
 def connect_to_db(app):
@@ -22,6 +24,41 @@ class PeerParticipants(db.Model):
         return '<PeerParticipants id=%r asn=%r name=%r info_traffic=%r>' %(
             self.id, self.asn, self.name, self.info_traffic)
 
+    def serialize(self):
+        """Returns object data that is serializable"""
+        return{
+
+            'id': self.id,
+             'asn': self.asn,
+             'name': self.name,
+             'aka': self.aka,
+             'website': self.website,
+             'notes_public': self.notes_public,
+             'notes_private': self.notes_private,
+             'irr_as_set': self.irr_as_set,
+             'info_traffic': self.info_traffic,
+             'info_ratio': self.info_ratio,
+             'info_scope': self.info_scope,
+             'info_type': self.info_type,
+             'info_prefixes': self.info_prefixes,
+             'info_lookingglass': self.info_lookingglass,
+             'info_routeserver': self.info_routeserver,
+             'info_unicast': self.info_unicast,
+             'info_multicast': self.info_multicast,
+             'info_ipv6': self.info_ipv6,
+             'policy_url': self.policy_url,
+             'policy_general': self.policy_general,
+             'policy_locations': self.policy_locations,
+             'policy_ratio': self.policy_ratio,
+             'policy_contracts': self.policy_contracts,
+             'policy_nopublic': self.policy_nopublic,
+             'policy_noprivate': self.policy_noprivate,
+             'date_created': self.date_created,
+             'date_lastupdated': self.date_lastupdated
+        }
+
+
+
 class MgmtPublics(db.Model):
     __tablename__ = 'mgmtPublics'
     __table_args__ = {'extend_existing': True}
@@ -31,6 +68,29 @@ class MgmtPublics(db.Model):
         return '<MgmtPublics id=%r name=%r city=%r country=%r>' %(
             self.id, self.name, self.city, self.country)
 
+    def serialize(self):
+        """Returns object data that is serializeable"""
+        return {
+
+             'id': self.id,
+             'approved': self.approved,
+             'name': self.name,
+             'name_long': self.name_long,
+             'ipaddr': self.ipaddr,
+             'city': self.city,
+             'country': self.country,
+             'region_continent': self.region_continent,
+             'media': self.media,
+             'tech_email': self.tech_email,
+             'tech_phone': self.tech_phone,
+             'policy_email': self.policy_email,
+             'policy_phone': self.policy_phone,
+             'website': self.website,
+             'url_stats': self.url_stats,
+             'proto_unicast': self.proto_unicast,
+             'proto_multicast': self.proto_multicast,
+             'proto_ipv6': self.proto_ipv6
+        }
 
 class PeerParticipantsPublics(db.Model):
     __tablename__ = 'peerParticipantsPublics'
@@ -48,6 +108,25 @@ class PeerParticipantsPublics(db.Model):
     def __repr__(self):
         return '<PeerParticipantsPublics id=%s local_asn=%s speed=%s>' % (
             self.id, self.local_asn, self.speed)
+
+    def serialize(self):
+        """Returns object data that is serializeable"""
+        return {
+
+             'id': self.id,
+             'participant_id': self.participant_id,
+             'public_id': self.public_id,
+             'local_asn': self.local_asn,
+             'local_ipaddr': self.local_ipaddr,
+             'speed': self.speed,
+             'protocol': self.protocol,
+             'pending': self.pending
+        }
+
+
+
+
+
 
 if __name__ == "__main__":
     from server import app
