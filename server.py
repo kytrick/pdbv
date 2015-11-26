cea05b938987d2116ee0d275fa4632d2e68a2f63
Kayla Smith <kytrick@gmail.com>
2015-11-25 20:35:39 -0800

    removed old search

--- server.py
+++ server.py
@@ -13,10 +13,10 @@ from flaregen import (
 from flask import Flask, render_template, request, url_for, redirect
 from flask.ext.bower import Bower
 from model import connect_to_db
-import re
 
 app = Flask(__name__)
-Bower(app) # This provides the /bower url route
+Bower(app)  # This provides the /bower url route
+
 
 @app.route('/')
 def index():
@@ -47,24 +47,10 @@ def collapsible_tree():
     return render_template('collapsible_tree.html',
                            current_asn=current_asn, flare_base=flare_base)
 
+
 @app.route('/livesearch/<query>')
 def typesearch(query):
     return '%s' % live_search(query)
-
-@app.route('/search')
-def search_function():
-    # consumes "search" from base.html form
-    # uses search as argument that goes into SQL
-    # gets back an asn
-    # returns a redirect to collapsible tree with that asn
-    search = request.args.get("search")
-    p = re.compile("^[0-9]+$")
-    if p.match(search):
-        return redirect(url_for("collapsible_tree", asn=search))
-    else:
-        asn = asn_search(search)
-        return redirect(url_for("collapsible_tree", asn=asn))
-
 
 
 if __name__ == "__main__":
